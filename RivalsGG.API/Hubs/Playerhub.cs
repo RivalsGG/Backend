85dc4c452d793212011f4a74e5a8c1e2e9e16ab5
Rick S <290278@student.fontys.nl>
2025-06-04 19:06:38 +0200

    Websocket setup

---
+++ RivalsGG.API/Hubs/Playerhub.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+using Microsoft.AspNetCore.SignalR;
+
+namespace RivalsGG.API.Hubs
+{
+    public class Playerhub : Hub
+    {
+        public override async Task OnConnectedAsync()
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, "PlayerUpdates");
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, "PlayerUpdates");
+            await base.OnDisconnectedAsync(exception);
+        }
+    }
+}
